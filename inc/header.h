e81b4e95792d3efea4ab799b0862e34c91e20553
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-02-27 22:45:55 +0200

    Update header.h добавили структуру клетки и функции

--- inc/header.h
+++ inc/header.h
@@ -15,6 +15,39 @@
 #define FPS 60
 #define FRAME_TIME (1000 / FPS)
 
+        //~~~//
+
+typedef struct cell {
+    int x;
+    int y;
+    bool cell_shown;
+    bool wall_up;
+    bool wall_up_shown;
+    bool wall_down;
+    bool wall_down_shown;
+    bool wall_right;
+    bool wall_right_shown;
+    bool wall_left;
+    bool wall_left_shown;
+    struct cell *river;
+    int river_num;
+    bool river_shown;
+    struct cell *portal;
+    int portal_num;
+    bool portal_shown;
+    int treasure; // -1 - нет клада, 0 - ложный, 1 - истинный
+}              cell;
+
+void mx_map(cell karta[10][10]);
+void mx_gen(cell map[10][10]);
+void mx_randyx(int *y, int *x);
+int mx_check_up(cell map[10][10], int y, int x);
+int mx_check_down(cell map[10][10], int y, int x);
+int mx_check_left(cell map[10][10], int y, int x);
+int mx_check_right(cell map[10][10], int y, int x);
+
+        //~~~//
+
 void mx_create_window(SDL_Window **window, SDL_Renderer **renderer);
 void mx_destroy_window(SDL_Window **window, SDL_Renderer **renderer);
 SDL_Texture *mx_change_background(const char* imagePath, SDL_Renderer **renderer);
