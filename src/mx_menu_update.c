ccfc783fa3ba0cf6f02c90155bd975cd7b700a59
ydmytruk <ydmytruk@stud.khpi.ucode-connect.study>
2024-03-01 18:45:46 +0200

    deleted unused files

--- src/mx_menu_update.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/header.h"
-
-void mx_menu_update(int *last_frame_time) {
-	int wait = FRAME_TIME - (SDL_GetTicks() - *last_frame_time);
-
-	if (wait > 0 && wait <= FRAME_TIME)
-		SDL_Delay(wait);
-}
-
